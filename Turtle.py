d0fcd8926e0a93a70525b20305a037b0eab52140
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-05-08 23:41:33 +0100

    Setup example

--- Turtle.py
+++ Turtle.py
@@ -3,11 +3,15 @@ from _turtle import _turtle # Include the ability to make a turtle.
 import time # Include the ability to stop time
 import numpy as np # Include the ability to use pi (3.14...)
 
+t = _turtle() # Initialise Turtle
 
 # Example to draw a octagon
+def drawExample(t):    
+    t.draw = True # make the turtle draw
+    for i in range(0,8):
+        t.move(50)
+        t.turn(np.pi/4)
+        time.sleep(.5)
+    time.sleep(2)
 
-t = _turtle() # Initialise Turtle
-t.draw = True # make the turtle draw
-
-
-
+drawExample(t)
